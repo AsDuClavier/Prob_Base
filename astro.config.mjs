0a9f2866558079b2a2e48fecfd82d278a1832217
AsDuClavier <91341258+AsDuClavier@users.noreply.github.com>
2026-07-17 15:58:32 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // Obligatoire pour GitHub Pages
-  site: 'https://<ton-pseudo>.github.io',
-  base: '/<nom-du-depot>', 
+  site: 'https://asduclavier.github.io',
+  base: '/Prob_base, 
   output: 'static', // Génère du HTML pur
 });
